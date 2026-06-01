b7cc5eca5f43389555a0f62127fa8ced2340083b
Henryvdat <neilw3d@gmail.com>
2026-05-31 20:24:47 -0400

    Initial Commit

---
+++ swiftNotation/swiftNotationApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftNotationApp.swift
+//  swiftNotation
+//
+//  Created by Neil Whittaker on 5/31/26.
+//
+
+import SwiftUI
+
+@main
+struct swiftNotationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftNotationTests/swiftNotationTests.swift
@@ -0,0 +1,19 @@
+//
+//  swiftNotationTests.swift
+//  swiftNotationTests
+//
+//  Created by Neil Whittaker on 5/31/26.
+//
+
+import Testing
+@testable import swiftNotation
+
+struct swiftNotationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
